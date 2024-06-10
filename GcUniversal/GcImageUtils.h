4a58961b636a1e8a44a6765b49d61e2cadda741a
MrGcGamer <32813275+MrGcGamer@users.noreply.github.com>
2024-06-10 17:52:20 +0200

    Update GcImageUtils.h

--- GcUniversal/GcImageUtils.h
+++ GcUniversal/GcImageUtils.h
@@ -2,7 +2,9 @@
 #import <UIKit/UIKit.h>
 
 FOUNDATION_EXPORT BOOL isHEICSupported(void);
+#if __IPHONE_OS_VERSION_MAX_ALLOWED < 170000
 FOUNDATION_EXPORT NSData *UIImageHEICRepresentation(UIImage *image, CGFloat compressionQuality);
+#endif
 
 @interface UIImage (imageUtils)
 + (UIImage *)stockImgForBundleID:(NSString *)arg1 ;
